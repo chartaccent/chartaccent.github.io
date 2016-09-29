8965c7357c0d7baa4429f00f287e8bcb199241d6
Donghao Ren <donghao.ren@gmail.com>
2016-09-29 16:21:10 -0700

    Update.

--- chartaccent.js
+++ chartaccent.js
@@ -12,7 +12,7 @@ var ChartAccent = ((function() {
 
     typeahead.loadjQueryPlugin();
 
-FONT_chartaccent_icons = {"chartaccent-icons-font-smaller": "\ue014", "chartaccent-icons-annotation": "\ue000", "chartaccent-icons-checkbox-checked": "\ue007", "chartaccent-icons-line-start": "\ue01b", "chartaccent-icons-minus": "\ue01d", "chartaccent-icons-circles": "\ue00b", "chartaccent-icons-shape-label": "\ue028", "chartaccent-icons-range": "\ue024", "chartaccent-icons-line-none": "\ue019", "chartaccent-icons-range-without": "\ue023", "chartaccent-icons-plus": "\ue01f", "chartaccent-icons-item-label": "\ue015", "chartaccent-icons-checkbox-correct-empty": "\ue009", "chartaccent-icons-range-within": "\ue022", "chartaccent-icons-line-above": "\ue016", "chartaccent-icons-arrow-right": "\ue003", "chartaccent-icons-line-start-end": "\ue01a", "chartaccent-icons-region": "\ue025", "chartaccent-icons-edit": "\ue011", "chartaccent-icons-shape-line": "\ue029", "chartaccent-icons-lines": "\ue01c", "chartaccent-icons-font-larger": "\ue013", "chartaccent-icons-arrow-left": "\ue002", "chartaccent-icons-blank": "\ue006", "chartaccent-icons-shape-arrow": "\ue026", "chartaccent-icons-preset": "\ue020", "chartaccent-icons-bars": "\ue005", "chartaccent-icons-arrow-up": "\ue004", "chartaccent-icons-arrow-down": "\ue001", "chartaccent-icons-digits-more": "\ue010", "chartaccent-icons-checkbox-empty": "\ue00a", "chartaccent-icons-shape-oval": "\ue02a", "chartaccent-icons-trendline": "\ue02d", "chartaccent-icons-digits-less": "\ue00f", "chartaccent-icons-shape-rect": "\ue02b", "chartaccent-icons-line-below": "\ue017", "chartaccent-icons-correct": "\ue00c", "chartaccent-icons-slash": "\ue02c", "chartaccent-icons-range-line": "\ue021", "chartaccent-icons-more": "\ue01e", "chartaccent-icons-checkbox-correct-checked": "\ue008", "chartaccent-icons-eye": "\ue012", "chartaccent-icons-line-end": "\ue018", "chartaccent-icons-cross-small": "\ue00d", "chartaccent-icons-shape-image": "\ue027", "chartaccent-icons-cross": "\ue00e"};
+FONT_chartaccent_icons = {"chartaccent-icons-font-smaller": "\ue015", "chartaccent-icons-annotation": "\ue000", "chartaccent-icons-checkbox-checked": "\ue007", "chartaccent-icons-line-start": "\ue01c", "chartaccent-icons-minus": "\ue01e", "chartaccent-icons-circles": "\ue00b", "chartaccent-icons-shape-label": "\ue029", "chartaccent-icons-range": "\ue025", "chartaccent-icons-line-none": "\ue01a", "chartaccent-icons-range-without": "\ue024", "chartaccent-icons-plus": "\ue020", "chartaccent-icons-item-label": "\ue016", "chartaccent-icons-checkbox-correct-empty": "\ue009", "chartaccent-icons-range-within": "\ue023", "chartaccent-icons-line-above": "\ue017", "chartaccent-icons-arrow-right": "\ue003", "chartaccent-icons-line-start-end": "\ue01b", "chartaccent-icons-region": "\ue026", "chartaccent-icons-edit": "\ue011", "chartaccent-icons-shape-line": "\ue02a", "chartaccent-icons-lines": "\ue01d", "chartaccent-icons-font-larger": "\ue014", "chartaccent-icons-arrow-left": "\ue002", "chartaccent-icons-blank": "\ue006", "chartaccent-icons-shape-arrow": "\ue027", "chartaccent-icons-export": "\ue012", "chartaccent-icons-preset": "\ue021", "chartaccent-icons-bars": "\ue005", "chartaccent-icons-arrow-up": "\ue004", "chartaccent-icons-arrow-down": "\ue001", "chartaccent-icons-digits-more": "\ue010", "chartaccent-icons-checkbox-empty": "\ue00a", "chartaccent-icons-shape-oval": "\ue02b", "chartaccent-icons-trendline": "\ue02e", "chartaccent-icons-digits-less": "\ue00f", "chartaccent-icons-shape-rect": "\ue02c", "chartaccent-icons-line-below": "\ue018", "chartaccent-icons-correct": "\ue00c", "chartaccent-icons-slash": "\ue02d", "chartaccent-icons-range-line": "\ue022", "chartaccent-icons-more": "\ue01f", "chartaccent-icons-checkbox-correct-checked": "\ue008", "chartaccent-icons-eye": "\ue013", "chartaccent-icons-line-end": "\ue019", "chartaccent-icons-cross-small": "\ue00d", "chartaccent-icons-shape-image": "\ue028", "chartaccent-icons-cross": "\ue00e"};
 
 // Make an object capable for accepting events.
 // Binding behaviour is similar to d3.
@@ -2091,6 +2091,7 @@ var MakeColorPicker = function(sel, color, onchange, clickout_handlers) {
         var element = this;
         var colorpicker = CreateEditorPopup("ColorPicker", {
             anchor: this,
+            align: "right",
             parent_clickout_handlers: clickout_handlers,
             color: color
         });
@@ -2133,6 +2134,7 @@ var MakeStrokeColorPicker = function(sel, color, onchange, clickout_handlers) {
         var element = this;
         var colorpicker = CreateEditorPopup("ColorPicker", {
             anchor: this,
+            align: "right",
             parent_clickout_handlers: clickout_handlers,
             color: color
         });
@@ -5671,13 +5673,14 @@ var MakeEditorPopupWrapper = function(info) {
         "top": (target_rect.top - body_rect.top + target_rect.height + 6) + "px"
     });
 
-    wrapper.append("div").classed("border-triangle", true);
+    var border_triangle = wrapper.append("div").classed("border-triangle", true);
 
     if(info.align == "right") {
         wrapper.style({
             "right": -(target_rect.right - body_rect.right) + "px",
             "left": null
         });
+        border_triangle.classed("align-right", true);
     }
     wrapper.style({
         "position": "absolute",
